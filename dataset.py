a8f713f943bd6f86311ba1f5a1abdd0eb7701dc3
Snir Lugassy <snirlugassy@yahoo.com>
2022-05-02 09:41:53 +0300

    first commit

---
+++ dataset.py
@@ -0,0 +1,19 @@
+import os
+from torch.utils.data.dataset import Dataset
+from torchvision.io import read_image
+
+class MaskImageDataset(Dataset):
+    def __init__(self, img_dir, transform=None):
+        self.dir = img_dir
+        self.image_files = [os.path.join(img_dir, i) for i in os.listdir(img_dir)]
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, idx):
+        image = read_image(self.image_files[idx])
+        label = int(os.path.splitext(self.image_files[idx])[0].split('_')[1])
+        if self.transform:
+            image = self.transform(image)
+        return image, label
---
+++ transform.py
@@ -0,0 +1,17 @@
+import torch
+from torchvision.transforms import InterpolationMode
+from torchvision.transforms import Resize
+from torchvision.transforms import RandomCrop
+from torchvision.transforms import RandomVerticalFlip
+from torchvision.transforms import RandomHorizontalFlip
+from torchvision.transforms import RandomGrayscale
+from torchvision.transforms import ColorJitter
+
+mask_image_transform = torch.nn.Sequential(
+    Resize((256,256), InterpolationMode.BILINEAR),
+    RandomCrop((224,224)),
+    RandomHorizontalFlip(),
+    RandomVerticalFlip(),
+    RandomGrayscale(),
+    ColorJitter(brightness=0.5, hue=0.5, contrast=0.5)
+)
