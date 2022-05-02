398564ff16cb9d077608a348b877c94896d2bddb
Snir Lugassy <snirlugassy@yahoo.com>
2022-05-02 15:29:44 +0300

    sdf

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import torch
 def calc_accuracy(classifier, loader, limit=None):
     correct = 0
     total = 0
-    for images, labels in loader:
+    for i, (images, labels) in enumerate(loader):
         outputs = classifier(images)
         predicted = torch.argmax(outputs, 1)
         total += len(labels)
